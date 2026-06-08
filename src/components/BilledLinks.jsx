fbd7ed6ea274da8e30357948c8d769b012d56fb0
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-06-08 16:10:30 +0200

    landing page done

---
+++ src/components/BilledLinks.jsx
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom'
+
+const BilledLinks = () => {
+  return (
+    <div className="grid grid-cols-3 lg:grid-cols-5 gap-8 mt-2 p-8 px-8 md:px-48 bg-neutral-900 border-t border-b border-green-700 shadow-md">
+      <Link to="/david" className="hover:scale-110 hover:shadow-lg transition-all ease-in-out duration-300 hover:cursor-pointer border border-primary">
+        <img src="/images/david.png" alt="David" className="w-full h-auto shadow-md" />
+      </Link>
+      <Link to="/villas" className="hover:scale-110 hover:shadow-lg transition-all ease-in-out duration-300 hover:cursor-pointer border border-primary">
+        <img src="/images/villas.png" alt="Villas" className="w-full h-auto shadow-md" />
+      </Link>
+      <Link to="/gulle" className="hover:scale-110 hover:shadow-lg transition-all ease-in-out duration-300 hover:cursor-pointer border border-primary">
+        <img src="/images/gulle.png" alt="Gulle" className="w-full h-auto shadow-md" />
+      </Link>
+      <Link to="/niko" className="hover:scale-110 hover:shadow-lg transition-all ease-in-out duration-300 hover:cursor-pointer border border-primary">
+        <img src="/images/niko.png" alt="Niko" className="w-full h-auto shadow-md" />
+      </Link>
+      <Link to="/eli" className="hover:scale-110 hover:shadow-lg transition-all ease-in-out duration-300 hover:cursor-pointer border border-primary">
+        <img src="/images/eli.png" alt="Eli" className="w-full h-auto shadow-md" />
+      </Link>
+    </div>
+  )
+}
+
+export default BilledLinks
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,9 @@
-
+import BilledLinks from "../components/BilledLinks"
 
 const Landing = () => {
   return (
     <>
-    <div className="text-left text-lg p-4 px-8 md:px-48 font-bold">
+    <div className="text-left text-lg p-4 px-8 mt-12 md:px-48 font-bold">
      <h1 className="text-2xl">
       The Official Risk Tournament of World Domination
      </h1>
@@ -11,10 +11,22 @@ const Landing = () => {
       2026 Edition
      </h2>
 
-     <p className="mt-12 font-regular text-md">
-      You have been granted access to a secret site, containing information about the upcoming Risk Tournament of World Domination. This is a prestigious event where players from around the world compete for global supremacy. The tournament will feature intense battles, strategic alliances, and thrilling gameplay as participants vie for the title of World Domination Champion. Stay tuned for updates and prepare your strategies for the ultimate Risk showdown!
+     <p className="mt-12 font-regular text-md text-primary mb-8">
+      You have been granted access to a secret site, 
+      containing information about the upcoming Risk Tournament of World Domination. 
+      This is a prestigious event where players compete for global supremacy. 
+      The tournament will feature alcohol, food, strategic alliances, and thrilling battles as participants compete for the title of <span className="text-secondary font-bold"> Risk World Champion of 2026. </span> 
+     </p>
+
+     <p className="mt-12 font-regular text-md text-primary mb-8">
+      Use this once in a lifetime chance to read up on your opponents, and prepare for a glorious victory.
      </p>
      </div>
+      <BilledLinks />
+
+      <p className="text-center text-md p-4 px-8 mt-12 mb-12 md:px-48 font-bold text-primary">
+      Good luck, and may the best player win.
+      </p>
     </>
   )
 }
